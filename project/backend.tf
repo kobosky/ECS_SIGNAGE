7f2a7d40714e43c40affc331d3a7b653d6ff913f
kobosky <kobosky_3@yahoo.com>
2023-08-11 01:51:34 +0100

    project

---
+++ project/backend.tf
@@ -0,0 +1,10 @@
+# store the terraform state file in s3 and lock with dynamodb
+terraform {
+  backend "s3" {
+    bucket         = "dosignage-terraform-state"
+    key            = "terraform-module/signage/terraform.tfstate"
+    region         = "eu-west-2"
+    #profile        = "terraform"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
---
+++ project/main.tf
@@ -0,0 +1,242 @@
+# create vpc
+resource "aws_vpc" "signage-vpc" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "signage-vpc"
+  }
+}
+
+# create internet gateway and attach it to vpc
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.signage-vpc.id
+
+  tags = {
+    Name = "signage-igw"
+  }
+}
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+# create public subnet az1
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id                  = aws_vpc.signage-vpc.id
+  cidr_block              = var.public_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "signage-public-az1"
+  }
+}
+
+# create public subnet az2
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = aws_vpc.signage-vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "signage-public-az2"
+  }
+}
+
+# create route table and add public route
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.signage-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "signage-public-rt"
+  }
+}
+
+# associate public subnet az1 to "public route table"
+resource "aws_route_table_association" "public_subnet_az1_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# associate public subnet az2 to "public route table"
+resource "aws_route_table_association" "public_subnet_az2_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# create private app subnet az1
+resource "aws_subnet" "private_app_subnet_az1" {
+  vpc_id                  = aws_vpc.signage-vpc.id
+  cidr_block              = var.private_subnet_app_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "signage-private-app-az1"
+  }
+}
+
+# create private app subnet az2
+resource "aws_subnet" "private_app_subnet_az2" {
+  vpc_id                  = aws_vpc.signage-vpc.id
+  cidr_block              = var.private_subnet_app_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "signage-private-app-az2"
+  }
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster" # Naming the cluster
+}
+
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "488777800893.dkr.ecr.eu-west-2.amazonaws.com/helloword:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# create application load balancer
+resource "aws_lb" "application_load_balancer" {
+  name                       = "test-lb-tf" # Naming our load balancer
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = ["${aws_security_group.load_balancer_security_group.id}"] # Referencing the load balancer security group
+  subnets                    = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "signage-alb"
+  }
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  name        = "signage-alb-sg"
+  description = "enable http/https access on port 80"
+  vpc_id = aws_vpc.signage-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_vpc.signage-vpc.id}" # Referencing the VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_lb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_subnet.public_subnet_az1.id}", "${aws_subnet.public_subnet_az2.id}"]
+    assign_public_ip = true                                                # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  name        = "service-sg"
+  description = "enable http access on port 80"
+  vpc_id      = aws_vpc.signage-vpc.id
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
